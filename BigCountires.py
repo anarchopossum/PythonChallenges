2e117f33518dbdc1d97cfb7e1de9da9238b9d8c0
anarchopossum <jasmine.sanjuan01@student.csulb.edu>
2023-08-20 22:00:19 -0700

    added comments to BigCountires.

--- BigCountires.py
+++ BigCountires.py
@@ -1,5 +1,9 @@
 import pandas as pd
 
+'''
+    The biggest difficulty I had with this Pandas refresher was to figure out the proper syntax to copy the data from the original dataframe and 
+    also set the constraints with it too. I have to make sure that i keep both my data and constraints in seperate brackets.
+'''
 def big_countries(world: pd.DataFrame) -> pd.DataFrame:
     data_filter = world[(world['population'] > 25000000) | (world['area'] > 3000000)][['name','population','area']]
     return data_filter
