9259c0ed0a7a52a34698c5af3d4b0bc4f5b27759
anarchopossum <jasmine.sanjuan01@student.csulb.edu>
2023-08-23 00:17:39 -0700

    Made isnotin.py comments more legible

--- isnotin.py
+++ isnotin.py
@@ -1,14 +1,20 @@
 import pandas as pd
 
 def find_customers(customers: pd.DataFrame, orders: pd.DataFrame) -> pd.DataFrame:
-    # # merge both tables and use the right table (customers) to compare
-    # output = orders.merge(customers, left_on='customerId', right_on='id', how='right')
-    # # grab all the values that have N/A on column 'customerId' and only keep column 'name'
-    # output = output[output.customerId.isna()][['name']]
-    # # Renames the 'name' to 'Customers'
-    # output = output.rename(columns={'name' : 'Customers'})
+    '''
+     # merge both tables and use the right table (customers) to compare
+    output = orders.merge(customers, left_on='customerId', right_on='id', how='right')
+     # grab all the values that have N/A on column 'customerId' and only keep column 'name'
+    output = output[output.customerId.isna()][['name']]
+     # Renames the 'name' to 'Customers'
+    output = output.rename(columns={'name' : 'Customers'})
+    '''
 
-    # Redo using isin(). We are using '~' to flip the 'isin()' to find all the values that are not in the orders 'customerId'. next we say we only want to keep the column 'name'. and finally change the column title from 'name' to 'Customers'
+
+    '''
+    Redo using isin(). We are using '~' to flip the 'isin()' to find all the values that are not in the orders'customerId'.
+    Next we say we only want to keep the column 'name'. and finally change the column title from 'name' to 'Customers'
+    '''
     output = customers[~customers.id.isin(orders['customerId'])][['name']].rename(columns={'name' : 'Customers'})
     return output
 
